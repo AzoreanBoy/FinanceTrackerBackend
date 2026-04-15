0f209aa43b4f9218da4f23728620495d1aa75ea1
AzoreanBoy <pedroregoferreira@gmail.com>
2026-04-15 19:05:13 +0100

    Implement CustomUserAdmin for improved admin interface and user display

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import CustomUser
 
 # Registered Models
-admin.site.register(CustomUser)
+
+class CustomUserAdmin(admin.ModelAdmin):
+
+    @admin.display(description='Full Name')
+    def full_name(self, obj: CustomUser) -> str:
+        return f"{obj.first_name} {obj.last_name}".strip() or obj.username
+
+    list_display = ('username','email', 'full_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
