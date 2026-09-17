a83cb35bd882a8bbd48549594baee0f12db8025e
AzoreanBoy <pedroregoferreira@gmail.com>
2026-09-17 23:45:21 +0100

    Registers BalanceCorrectionViewSet with the router for API routing

--- finance/routers.py
+++ finance/routers.py
@@ -1,6 +1,8 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet
+from .views import BalanceCorrectionViewSet, CategoryViewSet
 
 router = DefaultRouter()
+# Register de ModelsViewSet with the router
 router.register("categories", CategoryViewSet, basename="category")
+router.register("balance-corrections", BalanceCorrectionViewSet, basename="balance-correction")
