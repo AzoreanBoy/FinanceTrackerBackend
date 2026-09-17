c4882a47da9d7f4d8fa8bafa1b5a95339e5de4e9
AzoreanBoy <pedroregoferreira@gmail.com>
2026-09-17 08:28:12 +0100

    Configure REST framework and token authentication

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     
     # Third-party apps
     "rest_framework",
+    "rest_framework.authtoken",
     
     # Project apps
     "accounts",
@@ -136,4 +137,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
+    "DEFAULT_PERMISSION_CLASSES": [
+            "rest_framework.permissions.IsAuthenticated",
+        ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
 }
