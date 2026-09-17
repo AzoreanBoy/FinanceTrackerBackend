fcf2a059653343b9b5915dfca91efa7427ac1c2d
AzoreanBoy <pedroregoferreira@gmail.com>
2026-09-17 09:06:11 +0100

    Configures API routing for finance application

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/token/", obtain_auth_token),
     path("api/accounts/", include("accounts.urls")),
+    path("api/finance/", include("finance.urls")),
 ]
