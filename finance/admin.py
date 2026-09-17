adb87969381075d2370fa1cf85a963f0decf5145
AzoreanBoy <pedroregoferreira@gmail.com>
2026-09-17 08:28:12 +0100

    Register finance models in admin interface

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from finance.models import *
 
-# Register your models here.
+# admin.site.register(Account)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    def delete_queryset(self, request, queryset):
+        for transaction in queryset:
+            transaction.delete()
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("name", "balance", "owner")
+    search_fields = ("name", "owner__username")
