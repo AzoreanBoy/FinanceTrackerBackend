5c82090c49023d99bc4e7ca2819796ed15c0cd3e
AzoreanBoy <pedroregoferreira@gmail.com>
2026-09-18 07:53:35 +0100

    Adds API schema endpoints for OpenAPI documentation

--- config/urls.py
+++ config/urls.py
@@ -14,12 +14,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 urlpatterns = [
+    # Admin site
     path("admin/", admin.site.urls),
+    # Documentation and schema
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    # API endpoints
     path("api/auth/token/", obtain_auth_token),
     path("api/accounts/", include("accounts.urls")),
     path("api/finance/", include("finance.urls")),
